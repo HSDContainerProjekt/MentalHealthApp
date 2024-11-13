c2129377b8dd7092c3f5e5b72caab0eef8bc5c49
Jonas Kampshoff <57630335+JonasKampshoff@users.noreply.github.com>
2024-11-13 20:15:29 +0100

    small changes for the routines

--- lib/routine_tracking/routine_tracking_backbone.dart
+++ lib/routine_tracking/routine_tracking_backbone.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class RoutineTracking extends StatelessWidget {
   const RoutineTracking({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Routine Tracking')),
+    return ListView(
+      children: [
+        Center(child: Text(AppLocalizations.of(context)!.routineTitle)),
+        CurrentRoutine(),
+        CurrentRoutine(),
+        CurrentRoutine(),
+        Center(child: Text("Aktuelle Aufgabe")),
+        Center(child: Text("All deine Aufgaben")),
+      ],
+    );
+  }
+}
+
+class CurrentRoutine extends StatelessWidget {
+  const CurrentRoutine({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      padding: const EdgeInsets.all(3.0),
+      decoration: BoxDecoration(border: Border.all(color: Colors.blueAccent)),
+      child: Text("Test"),
     );
   }
 }
---
+++ lib/table_of_contents/table_of_contents_backbone.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TableOfContents extends StatelessWidget {
+  const TableOfContents({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Routine Tracking')),
+    );
+  }
+}
