af69a2483771fc8c859f3cd7cc9ab795b30910f3
Jan Dahlke <jan-dahlke@gmx.de>
2024-12-03 17:46:21 +0100

    updated db

--- lib/friend_collection/database/online_database.sql
+++ lib/friend_collection/database/online_database.sql
@@ -3,7 +3,14 @@ CREATE DATABASE FriendsOnlineDatabase;
 DROP TABLE Friends;
 CREATE TABLE Friends (
     FriendID int NOT NULL,
-    Name varchar(255),
-    Birthday varchar(255),
+    Name varchar(255),friendsfriendsFriendID
+    Birthday date,
     PRIMARY KEY (FriendID)
 );
+
+INSERT INTO Friends VALUES(
+	3,
+    'TEST1',
+    '2000-01-01');
+    
+COMMIT;
