0e919968ea70238178e8f3785c03b57a325108c7
Jan Dahlke <jan-dahlke@gmx.de>
2024-12-16 15:43:51 +0100

    added database values

--- lib/friend_collection/model/friend.dart
+++ lib/friend_collection/model/friend.dart
@@ -1,12 +1,38 @@
 class Friend {
   final int friendID;
   final String? name;
+  final String? nickname;
   final String? birthday;
+  final String? zodiacSign;
+  final String? animal;
+
+  final String? hairColor;
+  final String? eyecolor;
+  final String? favoriteColor;
+
+  final String? favoriteSong;
+  final String? favoriteFood;
+  final String? favoriteBook;
+  final String? favoriteFilm;
+  final String? favoriteAnimal;
+  final int? favoriteNumber;
 
   const Friend({
     required this.friendID,
     this.name,
+    this.nickname,
     this.birthday,
+    this.zodiacSign,
+    this.animal,
+    this.hairColor,
+    this.eyecolor,
+    this.favoriteColor,
+    this.favoriteSong,
+    this.favoriteFood,
+    this.favoriteBook,
+    this.favoriteFilm,
+    this.favoriteAnimal,
+    this.favoriteNumber
   });
 
   factory Friend.fromSqfliteDatabase(Map<String, dynamic> map) => Friend(
