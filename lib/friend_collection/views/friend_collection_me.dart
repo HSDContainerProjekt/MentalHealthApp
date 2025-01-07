569683e900c1f4ff6b74b3264030dccb02fc0f87
Lilli Wieth <lilli.wieth@study.hs-duesseldorf.de>
2025-01-07 12:13:25 +0100

    einrückungen angepasst reste aus merge konflikt gelöscht

--- lib/friend_collection/views/friend_collection_me.dart
+++ lib/friend_collection/views/friend_collection_me.dart
@@ -50,9 +50,11 @@ class FriendCollectionMe extends StatelessWidget {
                     DottedBorder(
                       color: Colors.black,                  
                       padding: paddingvalue,
-                  child: Column(children: [
-                    Row(children: [Text("Das ist mein(e) Lieblings:")],),
-                  ],)
+                  child: Column(
+                    children: [
+                      Row(children: [Text(AppLocalizations.of(context)!.headlineFavoriteBox)],),
+                    ],
+                  )
                 ), // Das ist mein(e) lieblings
                     IntrinsicWidth(
                       child: Padding(
@@ -78,80 +80,3 @@ class FriendCollectionMe extends StatelessWidget {
   }
 }
 
-class 
-CustomColorWidget extends StatefulWidget {
-  final IconData passedIcon;
-  final String iconText;
-  const 
-  CustomColorWidget(this.passedIcon, this.iconText, {super.key});
-
-  @override
-  State<
-  CustomColorWidget> createState() => _CustomColorPicker();
-}
-
-class _CustomColorPicker extends State<CustomColorWidget> {
-  Color pickedColor = Color.fromARGB(255, Random().nextInt(255), Random().nextInt(255), Random().nextInt(255));
-
-  void changeColor(newColor){
-    setState(() {
-      pickedColor = newColor;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(widget.iconText),
-        GestureDetector(
-          onTap: (){
-            print(pickedColor);
-            showDialog(
-            context: context,
-            builder: (BuildContext context) => Dialog(
-              child: Container(
-                margin: const EdgeInsets.all(10),
-                width: 200,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    BlockPicker(
-                      pickerColor: pickedColor, //default color
-                      onColorChanged: (Color color){ //on the color picked
-                        changeColor(color);
-                        Navigator.pop(context);
-                      }, 
-                    ),
-                    TextButton(
-                      onPressed: () => 
-                        {Navigator.pop(context)
-
-                        },
-                      child: const Text('close'),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            );
-          },
-          child:  
-            Container(
-              decoration: 
-                BoxDecoration(
-                  color: pickedColor,
-                  shape: BoxShape.circle,   
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 2.0
-                  ),
-                ),
-                child: Icon(widget.passedIcon),
-            )
-        )
-      ]
-    );
-  }
-}
